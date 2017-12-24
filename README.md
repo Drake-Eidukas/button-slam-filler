2c98a647491ac33678120be39d1cfdd27c41d05d
Drake Eidukas <eidukas2@illinois.edu>
2017-12-24 05:08:23 -0600

    Add boilerplate code + setup basic code to webscrape.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # button-slam-filler
 Database population scripts for Button Slam, an upcoming alexa skill. 
+
+#setup
+1. Install conda / python 3.6.
+2. Clone button-slam-filler into your workspace.
+3. Create a new conda environment based on the environmnt file that I export.  
+
+   Use the command ```conda env create -f environment.yml``` to install the python dependencies that I use. 
+
+   To export the existing environment, use ```conda env export > environment.yml```
---
+++ dbFiller.py
@@ -0,0 +1,54 @@
+import json
+from bs4 import BeautifulSoup as bs
+import requests
+
+class InvalidIndex(Exception):
+    pass
+
+def get_webpage_data(question_id):
+    webpage = 'https://willyoupressthebutton.com/{0}/stats'.format(question_id)
+    request = requests.get(
+        webpage, 
+        headers={
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+        }
+    )
+    
+    soup = BeautifulSoup(request.text, 'html.parser')
+    
+    main_container = soup.find(id='maincontainer')
+    
+    if main_container is None:
+        raise InvalidIndex({
+            "message":"No question found with that index", 
+            "index": question_id
+        })
+    
+    stats = [stat for stat in [a for a in main_container.find(id='statsBar').children][1].children]
+    
+    did_press = stats[1].getText()
+    did_press_count = int(did_press.split()[0])
+    
+    didnt_press = stats[3].getText()
+    didnt_press_count = int(didnt_press.split()[0])
+
+    dilemma = [a for a in main_container.find(id='dilemma').children]
+    pro = dilemma[1].getText().strip()
+    con = dilemma[5].getText().strip()
+    
+    return {
+        'link': webpage,
+        'index': question_id,
+        'pro': pro,
+        'con': con,
+        'did_press_count': did_press_count,
+        'didnt_press_count': didnt_press_count
+    }
+    
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    print("Received context: ",  context)
+    for index in range(10000, 10010):
+        print(get_webpage_data(index))
+    # return event['key1']  # Echo back the first key value
+    #raise Exception('Something went wrong')
